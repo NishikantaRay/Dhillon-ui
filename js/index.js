fc263d0ef54206f6a2b4386dbc501b0abdf94e44
Nishikantaray <nishikantaray1@gmail.com>
2022-03-06 22:57:10 +0530

    search bar logic

--- js/index.js
+++ js/index.js
@@ -6,23 +6,30 @@ const getjsonData = async () => {
     try {
         let realdata = await fetch(api);
         data = await realdata.json();
-        // console.log(data);
+        console.log(data);
         // document.getElementById("lyrics").append( `${realData[0].lyrics}`);
-        
-let d = JSON.parse(data)
-        search_animal(realdata);
+        search_animal(data);
     } catch (error) {
         console.log(error);
     }
 };
 getjsonData();
   function search_animal(data) {
-      console.log(data);
     let input = document.getElementById('searchbar').value
     input = input.toLowerCase();
     let x = document.querySelector('#list-holder');
-    x.innerHTML = input;
-  
+    x.innerHTML = "";
+  console.log(data);
+    for (let index = 0; index < data.length; index++) {
+        if (data[index].title.toLowerCase().indexOf(input) != -1) {
+            x.innerHTML += `<div class="list-item">
+            <h3>${data[index].name}</h3>
+            <p>${data[index].description}</p>
+            <img src="${data[index].image}" alt="${data[index].name}">
+            </div>`;
+        }
+        
+    }
     // for (i = 0; i < data.length; i++) {
     //   let obj = data[i];
     //   if (obj.Name.toLowerCase().includes(input)) {
