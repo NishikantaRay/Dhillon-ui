9fcca49010fcca87fbc704ea11318c54f20479b3
Nishikantaray <nishikantaray1@gmail.com>
2022-03-06 22:49:20 +0530

    search bar

--- js/index.js
+++ js/index.js
@@ -4,11 +4,31 @@
 const getjsonData = async () => {
     const api = "./data/data.json";
     try {
-        let data = await fetch(api);
-        realData = await data.json();
-        // console.log(realData);
+        let realdata = await fetch(api);
+        data = await realdata.json();
+        // console.log(data);
+        // document.getElementById("lyrics").append( `${realData[0].lyrics}`);
+        
+let d = JSON.parse(data)
+        search_animal(realdata);
     } catch (error) {
         console.log(error);
     }
 };
 getjsonData();
+  function search_animal(data) {
+      console.log(data);
+    let input = document.getElementById('searchbar').value
+    input = input.toLowerCase();
+    let x = document.querySelector('#list-holder');
+    x.innerHTML = input;
+  
+    // for (i = 0; i < data.length; i++) {
+    //   let obj = data[i];
+    //   if (obj.Name.toLowerCase().includes(input)) {
+    //     const elem = document.createElement("li")
+    //     elem.innerHTML = `${obj.Name} - ${obj.Color}`
+    //     x.appendChild(elem)
+    //   }
+    // }
+  }
