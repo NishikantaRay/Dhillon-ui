c4697c57c5bf6076483b10d1c59044a6c6f4764d
Nishikantaray <nishikantaray1@gmail.com>
2022-03-07 00:25:29 +0530

    added music card

--- js/index.js
+++ js/index.js
@@ -31,10 +31,12 @@ const search_animal=async()=> {
             <div class="container pt-5">
     <div class="row">
         <div class="col-md-6 col-12">
+        <h3>${data[index].title}</h3>
         <a href="http://www.youtube.com/watch?v=${data[index].video_link}">
         <img src="https://i3.ytimg.com/vi/${data[index].video_link}/0.jpg" alt="Image"></a>
+        <h4>${data[index].singer}</h4>
         </div>
-        <div class="col-md-6 col-12">
+        <div class="col-md-6 col-12 box pt-5">
         <p>${data[index].lyrics}</p>
         </div>
     </div>
