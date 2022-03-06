76660650232bddca652b07b1b395deb2d69ae457
Nishikantaray <nishikantaray1@gmail.com>
2022-03-06 23:56:59 +0530

    added music card

--- js/index.js
+++ js/index.js
@@ -30,7 +30,7 @@ const search_animal=async()=> {
             x.innerHTML += `<div class="card pt-5">
             <center>
             <h2>Music Name</h2>
-            <img src="https://images.8tracks.com/cover/i/000/313/145/anime-music-widescreen-original-693454-3168.jpg?rect=0,0,500,500&q=98&fm=jpg&fit=max" alt="Image">
+            <img src="https://i3.ytimg.com/vi/cqP8I5aaud8/0.jpg" alt="Image">
           <audio controls>
             <source src="https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3" type="audio/mpeg">
             Your browser does not support the audio tag.
