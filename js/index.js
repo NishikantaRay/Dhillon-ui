bbe9dfa2ca777f192896ca47a664639ec434ca2b
Nishikantaray <nishikantaray1@gmail.com>
2022-03-07 01:03:42 +0530

    added music card

--- js/index.js
+++ js/index.js
@@ -1,32 +1,18 @@
 // for getting the data from the json file
 //credit -https://github.com/akaushik759
 
-const getjsonData = async () => {
-    const api = "./data/data.json";
-    try {
-        let realdata = await fetch(api);
-        data = await realdata.json();
-        console.log(data);
-    } catch (error) {
-        console.log(error);
-    }
-    
-    
-};
-getjsonData();
-
 // search bar function
-const search_song=async()=> {
+const search_song = async () => {
     let realdata = await fetch("./data/data.json");
     data = await realdata.json();
-    let input = document.getElementById('searchbar').value
+    let input = document.getElementById("searchbar").value;
     input = input.toLowerCase();
-    let x = document.querySelector('#list-holder');
+    let x = document.querySelector("#list-holder");
     x.innerHTML = "";
     for (let index = 0; index < data.length; index++) {
         if (data[index].title.toLowerCase().includes(input) && input != "") {
             x.innerHTML += `
-            <div class="container pt-5">
+            <div class="container boxcont pt-5">
     <div class="row">
         <div class="col-md-6 col-12">
         <h3>${data[index].title}</h3>
@@ -42,4 +28,4 @@ const search_song=async()=> {
             `;
         }
     }
-  }
+};
