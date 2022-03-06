179ba12c03d0bcf001d3b8af8366122eaccea680
Nishikantaray <nishikantaray1@gmail.com>
2022-03-06 22:15:27 +0530

    fetch the json file

--- js/index.js
+++ js/index.js
@@ -1,14 +1,14 @@
+// for getting the data from the json file
+//credit -https://github.com/akaushik759
 
-const getData = async () => {
-    const api = "./data.json";
+const getjsonData = async () => {
+    const api = "./data/data.json";
     try {
         let data = await fetch(api);
         realData = await data.json();
-        console.log(realData);
+        // console.log(realData);
     } catch (error) {
         console.log(error);
     }
 };
-
-
-getData();
+getjsonData();
